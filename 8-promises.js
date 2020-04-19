7a64ebad7f55fbac5ee3b398e3eb5e17ee5859d2
Anirup Patnaik <anirup049@gmail.com>
2020-04-19 17:17:10 +0530

    Promise chaining

--- 8-promises.js
+++ 8-promises.js
@@ -13,3 +13,36 @@ doWorkCallback.then((result) => {
 })
 
 // promise -- pending --> then its either fulfilled or rejected
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+// add(2,3).then((res) => {
+//     console.log(res)
+//     // Here if we want to perform Promise chanining then
+//     // we can do so in this manner but it will become more complex
+
+//     add(5, 5).then((res) => {
+//         console.log(res)
+//     }).catch((e) => {
+//         console.log(e)
+//     })
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+// Better and icer way to chain Promise calls
+
+add(2,3).then((res) => {
+    console.log(res)
+    return add(res, 4)
+}).then((finalSum) => {
+    console.log(finalSum)
+}).catch((e) => {
+    console.log(e)
+})
